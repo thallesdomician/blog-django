d865a657234e88457019e2e2c629e8ff21bafbef
Thalles Domician <thalles.oliveira@pravaler.com.br>
2021-02-01 22:59:19 -0300

    update gitignore

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/local/IDEALINVEST/thalles.oliveira/Documentos/thalles/BlogDjango/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

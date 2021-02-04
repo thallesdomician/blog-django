5b40cf6957a8161414d74575ddf1f7673b61362b
Thalles Domician <thalles.oliveira@pravaler.com.br>
2021-02-04 02:26:29 -0300

    features new structure forms, new translations, dinamics blocks

--- post/forms.py
+++ post/forms.py
@@ -1,2 +1,20 @@
-from django.forms import ModelForm
-from .models import Post, Tag
+from django import forms
+from .models import Post
+
+from django.utils.translation import gettext as _
+
+
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'id': 'editor'}), label=_('Content'))
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'subtitle',
+            'header_image',
+            'thumbnail_image',
+            'resume',
+            'content',
+            'categories'
+        ]
--- post/views.py
+++ post/views.py
@@ -7,6 +7,7 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post, Category
+from .forms import PostForm
 from django.shortcuts import render
 from django.utils.translation import gettext as _
 
@@ -22,22 +23,37 @@ class PostList(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['subheader'] = _('Blog')
         context['categories'] = Category.objects.filter(post__isnull=False).order_by('name')
         context['latest'] = Post.objects.all().order_by('-created_at')[:2]
-        context['subheader'] = _('Blog')
+        context['aside'] = True
         return context
 
 class PostCreate(CreateView):
     model = Post
     fields = ['title', 'subtitle', 'header_image', 'thumbnail_image', 'resume', 'content', 'categories']
     template_name_suffix = '_create_form'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['subheader'] = _('Create Post')
+        context['categories'] = Category.objects.filter(post__isnull=False).order_by('name')
+        context['latest'] = Post.objects.all().order_by('-created_at')[:2]
+        context['aside'] = True
+        return context
     def get_success_url(self, **kwargs):
         return reverse_lazy("post_detail", args=(self.object.slug,))
 
 class PostUpdate(UpdateView):
     model = Post
-    fields = ['title', 'subtitle', 'header_image', 'thumbnail_image', 'resume', 'content', 'categories']
+    form_class = PostForm
     template_name_suffix = '_update_form'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['subheader'] = _('Update Post')
+        context['categories'] = Category.objects.filter(post__isnull=False).order_by('name')
+        context['latest'] = Post.objects.all().order_by('-created_at')[:2]
+        context['aside'] = True
+        return context
     def get_success_url(self, **kwargs):
         return reverse_lazy("post_detail", args=(self.object.slug,))
 

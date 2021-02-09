a8f466348a164cc8f8c2370d9e1ccce577313434
Thalles Domician <thalles.oliveira@pravaler.com.br>
2021-02-09 00:42:57 -0300

    nova versao com alteracoes no admin

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 # Register your models here.
-from .models import Post, Category
+from django.utils.translation import ugettext_lazy as _
+from .models import Post, PostAdmin, Category, CategoryAdmin
 
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_header = _('Marcos\' Blog')
+admin.site.index_title = _('Administration')
+admin.site.site_title = _('Marcos\' Blog')
--- post/models.py
+++ post/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib import admin
 from colorfield.fields import ColorField
-from django.utils.translation import gettext as _
+from django.utils.translation import ugettext_lazy as _
 from django.template.defaultfilters import slugify
 from tinymce.models import HTMLField
 
@@ -10,8 +11,10 @@ class Category(models.Model):
         ('w', 'white'),
         ('b', 'black'),
     )
+
     class Meta:
         verbose_name_plural = _('Categories')
+
     name = models.CharField(max_length=15)
     color_text = models.CharField(max_length=1, choices=TEXT_COLOR, default=TEXT_COLOR[1][0])
     color_background = ColorField(default='#A5F8CE')
@@ -19,25 +22,81 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    fields = ('name',)
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=255)
-    header_image = models.ImageField(upload_to='header/%Y/%m/', null=True, blank=True)
-    thumbnail_image = models.ImageField(upload_to='thumbnail/%Y/%m/', null=True, blank=True)
-    resume = models.TextField(blank=True, null=True)
-    content = HTMLField(blank=True, null=True)
-    categories = models.ManyToManyField(Category)
+    title = models.CharField(max_length=100, verbose_name=_('Title'))
+    subtitle = models.CharField(max_length=255, verbose_name=_('Subtitle'))
+    header_image = models.ImageField(upload_to='header/%Y/%m/', null=True, blank=True, verbose_name=_('Header Image'))
+    thumbnail_image = models.ImageField(upload_to='thumbnail/%Y/%m/', null=True, blank=True, verbose_name=_('Thumbnail Image'))
+    resume = models.TextField(blank=True, null=True, verbose_name=_('Resume'))
+    content = HTMLField(blank=True, null=True, verbose_name=_('Content'))
+    categories = models.ManyToManyField(Category, verbose_name=_('Categories'))
     slug = models.SlugField(max_length=100, unique=True, default="slug", editable=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created'))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated'))
+
+
+
 
     def __str__(self):
         return self.title
+
+    def get_categories(self):
+        categories = self.categories.all()
+        return ', '.join([x.name for x in categories])
+
+    get_categories.short_description = _('Categories')
+
+
+    @property
+    def header_image_url(self):
+        if self.header_image and hasattr(self.header_image, 'url'):
+            return self.header_image.url
+
+    @property
+    def thumbnail_image_url(self):
+        if self.thumbnail_image and hasattr(self.thumbnail_image, 'url'):
+            return self.thumbnail_image.url
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super(Post, self).save(*args, **kwargs)
 
+    def has_thumbnail_image(self):
+        return True if self.thumbnail_image else False
+    has_thumbnail_image.boolean = True
+    has_thumbnail_image.short_description = _('Thumbnail')
 
 
+    def has_header_image(self):
+        return True if self.header_image else False
+    has_header_image.boolean = True
+    has_header_image.short_description = _('Image')
+
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (_('Headers'), {
+            'fields': (('title', 'subtitle'),),
+        }),
+        (_('Images'), {
+            'fields': (('header_image', 'thumbnail_image',),)
+        }),
+        (None, {
+            'fields': ('resume', 'content', 'categories',)
+        }),
+        (_('Dates'), {
+            'fields': (('created_at', 'updated_at', ),),
+            'classes': ['collapse', ]
+        })
+    )
+    autocomplete_fields = ('categories',)
+
+    list_display = ('title', 'subtitle', 'has_header_image', 'has_thumbnail_image', 'get_categories', 'created_at')
+    search_fields = ('title', 'subtitle', 'resume', 'content')
+    list_filter = ('categories__name',)
+
+    readonly_fields = ('created_at', 'updated_at')
